7909523aa9cf48d160f14f7f1f04c2c9b3068a7e
DiSaK <disak74@gmail.com>
2019-02-23 18:07:32 +0100

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+// Guardar en el cache dinamico
+function actualizaCacheDinamico(dynamic, req, res) {
+    if (res.ok) {
+        return caches.open(dynamic).then(cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        });
+    }else{
+        return res;
+    }
+}   
