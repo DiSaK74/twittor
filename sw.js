48482e83e1453d296cd9d7d696b0c8d0a9308e33
DiSaK <disak74@gmail.com>
2019-02-24 12:51:49 +0100

    Adaptaciones a Ios

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 // Imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    // '/',
+    // '/',cls
     'index.html',
     'css/style.css',
     'img/favicon.ico',
